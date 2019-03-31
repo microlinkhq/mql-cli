c8b8472b9700c0183ec7cfe4acecc011e92b8a74
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-03-31 23:48:05 +0200

    build: update example

--- examples/twitter.js
+++ examples/twitter.js
@@ -12,53 +12,64 @@ module.exports = async ({ query }) => {
   const { data } = await mql(`https://twitter.com/${username}`, {
     force,
     rules: {
+      stats: {
+        selector: '.ProfileNav-list',
+        attr: {
+          tweets: {
+            selector: '.ProfileNav-item--tweets .ProfileNav-value',
+            attr: 'data-count'
+          },
+          followings: {
+            selector: '.ProfileNav-item--following .ProfileNav-value',
+            attr: 'data-count'
+          },
+          favorites: {
+            selector: '.ProfileNav-item--favorites .ProfileNav-value',
+            attr: 'data-count'
+          },
+          moments: {
+            selector: '.ProfileNav-item--moments .ProfileNav-value',
+            attr: 'text'
+          }
+        }
+      },
       avatarUrl: {
         type: 'image',
-        selectors: {
-          selector: '.ProfileAvatar-image',
-          attr: 'src'
-        }
+        selector: '.ProfileAvatar-image',
+        attr: 'src'
       },
       bio: {
-        selectors: {
-          selector: '.ProfileHeaderCard-bio',
-          attr: 'text'
-        }
+        selector: '.ProfileHeaderCard-bio',
+        attr: 'text'
       },
       name: {
-        selectors: {
-          selector: '.ProfileHeaderCard-nameLink',
-          attr: 'text'
-        }
+        selector: '.ProfileHeaderCard-nameLink',
+        attr: 'text'
       },
       tweets: {
-        selectors: {
-          selector: 'ol > li',
-          attr: {
-            id: {
-              selector: '.tweet-timestamp',
-              attr: 'data-conversation-id',
-              type: value => `https://twitter.com/${value}`
-            },
-            text: {
-              selector: '.tweet-text',
-              attr: 'text'
-            },
-            tweetUrl: {
-              selector: '.tweet-timestamp',
-              attr: 'href'
-            }
+        selector: 'ol > li',
+        attr: {
+          id: {
+            selector: '.tweet-timestamp',
+            attr: 'data-conversation-id',
+            type: value => `https://twitter.com/${value}`
+          },
+          text: {
+            selector: '.tweet-text',
+            attr: 'text'
+          },
+          tweetUrl: {
+            selector: '.tweet-timestamp',
+            attr: 'href'
           }
         }
       }
     }
   })
 
-  const { tweets, bio, name, avatarUrl } = data
-
+  const { stats, tweets, bio, name, avatarUrl } = data
   const [pinnedTweet, ...restTweets] = tweets
-
-  return { pinnedTweet, tweets: restTweets, bio, name, avatarUrl }
+  return { pinnedTweet, tweets: restTweets, bio, name, avatarUrl, stats }
 }
 
 module.exports.help = 'Get the Twitter profile for any twitter username.'
